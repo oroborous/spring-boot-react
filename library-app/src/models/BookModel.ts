3904ba6634464ccbb815ab63094c9f2f4fdc66a5
Stacy Read <sread@wctc.edu>
2023-02-22 12:41:53 -0600

    Typescript BookModel created

---
+++ library-app/src/models/BookModel.ts
@@ -0,0 +1,30 @@
+class BookModel {
+    id: number;
+    title: string;
+    author?: string; // ? means nullable
+    description?: string;
+    copies?: number;
+    copiesAvailable?: number;
+    category?: string;
+    image?: string;
+
+    constructor(id: number,
+                title: string,
+                author: string,
+                description: string,
+                copies: number,
+                copiesAvailable: number,
+                category: string,
+                image: string) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.copies = copies;
+        this.copiesAvailable = copiesAvailable;
+        this.category = category;
+        this.image = image;
+    }
+}
+
+export default BookModel;
