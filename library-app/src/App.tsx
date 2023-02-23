75d9adf406e3076d7ed056ce4e57853d102409d4
Stacy Read <sread@wctc.edu>
2023-02-23 14:21:24 -0600

    Added react router

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -7,14 +7,24 @@ import {HomePage} from "./layout/homepage/HomePage";
 import {Navbar} from "./layout/navbar-and-footer/Navbar";
 import {Footer} from "./layout/navbar-and-footer/Footer";
 import {SearchBooksPage} from "./layout/search-books-page/SearchBooksPage";
+import {Redirect, Route, Switch} from "react-router-dom";
 
 
 export const App = () => {
     return (
         <div>
             <Navbar/>
-            {/*<HomePage/>*/}
-            <SearchBooksPage/>
+            <Switch>
+                <Route path="/" exact>
+                    <Redirect to="/home"/>
+                </Route>
+                <Route path="/home">
+                    <HomePage/>
+                </Route>
+                <Route path="/search">
+                    <SearchBooksPage/>
+                </Route>
+            </Switch>
             <Footer/>
         </div>
     );
