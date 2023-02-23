2fbfdc91b0dd0580bd8e5f2b4d9260dd16a10aa7
Stacy Read <sread@wctc.edu>
2023-02-23 15:01:01 -0600

    Added useeffect to load one book

--- library-app/src/layout/book-checkout-page/BookCheckoutPage.tsx
+++ library-app/src/layout/book-checkout-page/BookCheckoutPage.tsx
@@ -1,13 +1,51 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import BookModel from "../../models/BookModel";
+import {SpinnerLoading} from "../utils/SpinnerLoading";
 
 export const BookCheckoutPage = () => {
 
     const [book, setBook] = useState<BookModel>();
-    const [isLoadingBook, setIsLoadingBook] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
 
     const bookId = (window.location.pathname).split("/").pop();
+
+    // Will be called initially, and when any of the [] variables changes
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+            const response = await fetch(baseUrl); // raw text
+
+            if (!response.ok)
+                throw new Error("Something went wrong");
+
+            const responseJson = await response.json(); // JS object
+
+            const loadedBook: BookModel = responseJson;
+
+            setBook(loadedBook);
+            setIsLoading(false);
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <SpinnerLoading/>
+        );
+    }
+
+    if (httpError) {
+        return (
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        );
+    }
 
     return (
       <div>
