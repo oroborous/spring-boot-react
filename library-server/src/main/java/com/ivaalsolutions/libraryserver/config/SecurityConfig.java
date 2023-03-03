b92aa0d4733cccae9f205473f889a863ac15ced1
Stacy Read <sread@ivaalsolutions.com>
2023-03-03 14:12:30 -0600

    Authentication seems to be working for secure pages only; extract user email from jwt

--- library-server/src/main/java/com/ivaalsolutions/libraryserver/config/SecurityConfig.java
+++ library-server/src/main/java/com/ivaalsolutions/libraryserver/config/SecurityConfig.java
@@ -4,25 +4,35 @@ import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // Disable CSRF (Cross site request forgery)
-        http.csrf().disable();
-
-        // Protect endpoints at /api/<type>/secure
+//        // Disable CSRF (Cross site request forgery)
+//        http.csrf().disable();
+//
+//        // Protect endpoints at /api/<type>/secure
+//        http
+//                .authorizeHttpRequests(configurer ->
+//                        configurer
+//                                .requestMatchers("/api/books/secure/**")
+//                                .authenticated())
+//                .oauth2ResourceServer()
+//                .jwt();
         http
-                .authorizeHttpRequests(configurer ->
-                        configurer
-                                .requestMatchers("/api/books/secure/**")
-                                .authenticated())
-                .oauth2ResourceServer()
-                .jwt();
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/books/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
 
         // Add CORS filters to API endpoints
         http.cors();
--- library-server/src/main/java/com/ivaalsolutions/libraryserver/controller/BookController.java
+++ library-server/src/main/java/com/ivaalsolutions/libraryserver/controller/BookController.java
@@ -2,6 +2,7 @@ package com.ivaalsolutions.libraryserver.controller;
 
 import com.ivaalsolutions.libraryserver.entity.Book;
 import com.ivaalsolutions.libraryserver.service.BookService;
+import com.ivaalsolutions.libraryserver.utils.ExtractJwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,7 +22,7 @@ public class BookController {
     @GetMapping("/secure/ischeckedout/byuser")
     public Boolean checkoutBookByUser(@RequestParam Long bookId,
                                       @RequestHeader(value = "Authorization") String token) {
-        String userEmail = "testuser@email.com";
+        String userEmail = ExtractJwt.payloadJwtExtraction(token); //"testuser@email.com";
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
