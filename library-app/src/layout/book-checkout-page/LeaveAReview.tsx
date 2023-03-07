b1670e7d1b35ba1a6c1e03251527d4dd28fcaeb0
Stacy Read <sread@ivaalsolutions.com>
2023-03-07 14:23:12 -0600

    Description field appears when star rating is chosen

--- library-app/src/layout/book-checkout-page/LeaveAReview.tsx
+++ library-app/src/layout/book-checkout-page/LeaveAReview.tsx
@@ -3,9 +3,12 @@ import {StarsReview} from "../utils/StarsReview";
 
 export const LeaveAReview: React.FC<{}> = (props) => {
     const [starInput, setStarInput] = useState(0);
+    const [displayInput, setDisplayInput] = useState(false);
+    const [reviewDescription, setReviewDescription] = useState("");
 
     function starValue(value: number) {
         setStarInput(value);
+        setDisplayInput(true);
     }
 
     return (<div className="dropdown" style={{cursor: "pointer"}}>
@@ -13,18 +16,55 @@ export const LeaveAReview: React.FC<{}> = (props) => {
             Leave a review?
         </h5>
         <ul id="submitReviewRating" className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-            <li><button onClick={() => starValue(0)} className="dropdown-item">0 star</button></li>
-            <li><button onClick={() => starValue(0.5)} className="dropdown-item">0.5 star</button></li>
-            <li><button onClick={() => starValue(1)} className="dropdown-item">1 star</button></li>
-            <li><button onClick={() => starValue(1.5)} className="dropdown-item">1.5 star</button></li>
-            <li><button onClick={() => starValue(2)} className="dropdown-item">2 star</button></li>
-            <li><button onClick={() => starValue(2.5)} className="dropdown-item">2.5 star</button></li>
-            <li><button onClick={() => starValue(3)} className="dropdown-item">3 star</button></li>
-            <li><button onClick={() => starValue(3.5)} className="dropdown-item">3.5 star</button></li>
-            <li><button onClick={() => starValue(4)} className="dropdown-item">4 star</button></li>
-            <li><button onClick={() => starValue(4.5)} className="dropdown-item">4.5 star</button></li>
-            <li><button onClick={() => starValue(5)} className="dropdown-item">5 star</button></li>
+            <li>
+                <button onClick={() => starValue(0)} className="dropdown-item">0 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(0.5)} className="dropdown-item">0.5 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(1)} className="dropdown-item">1 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(1.5)} className="dropdown-item">1.5 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(2)} className="dropdown-item">2 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(2.5)} className="dropdown-item">2.5 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(3)} className="dropdown-item">3 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(3.5)} className="dropdown-item">3.5 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(4)} className="dropdown-item">4 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(4.5)} className="dropdown-item">4.5 star</button>
+            </li>
+            <li>
+                <button onClick={() => starValue(5)} className="dropdown-item">5 star</button>
+            </li>
         </ul>
         <StarsReview rating={starInput} size={32}/>
+        {displayInput &&
+            <form method="POST" action="#">
+                <hr/>
+                <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                        Description
+                    </label>
+                    <textarea id="submitReviewDescription" rows={3} placeholder="Optional"
+                              className="form-control" onChange={e => setReviewDescription(e.target.value)}></textarea>
+                </div>
+                <div>
+                    <button type="button" className="btn btn-primary mt-3">Submit Review</button>
+                </div>
+            </form>
+        }
     </div>);
 }
