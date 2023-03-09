a49217e7824964510a2aff58493a5de748b594fc
Stacy Read <sread@ivaalsolutions.com>
2023-03-09 13:50:41 -0600

    Create endpoint to fetch current loans for user

--- library-server/src/main/java/com/ivaalsolutions/libraryserver/controller/BookController.java
+++ library-server/src/main/java/com/ivaalsolutions/libraryserver/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.ivaalsolutions.libraryserver.controller;
 
 import com.ivaalsolutions.libraryserver.entity.Book;
+import com.ivaalsolutions.libraryserver.responsemodels.ShelfCurrentLoansResponse;
 import com.ivaalsolutions.libraryserver.service.BookService;
 import com.ivaalsolutions.libraryserver.utils.ExtractJwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -13,7 +16,14 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    // "sub" is subject of bearer token
     private final String SUB = "\"sub\"";
+
+    @GetMapping("/secure/currentloans")
+    public List<ShelfCurrentLoansResponse> currentLoans(@RequestHeader(value="Authorization") String token) throws Exception {
+        String userEmail = ExtractJwt.payloadJwtExtraction(token, SUB);
+        return bookService.currentLoans(userEmail);
+    }
 
     @GetMapping("/secure/currentloans/count")
     public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
